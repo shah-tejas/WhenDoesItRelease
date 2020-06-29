af26a917e8cf281a44d397f5dcbb1035ad8a7911
Tejas Shah <shah.te@northeastern.edu>
2020-06-29 11:15:52 -0400

    Updates

---
+++ precache-manifest.5b4da4ce65d8efb48ba72dd904790776.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e19f739ff58f07d4a2a0825812a7c8f",
+    "url": "/WhenDoesItRelease/index.html"
+  },
+  {
+    "revision": "82986302af2e91dc2483",
+    "url": "/WhenDoesItRelease/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b21d6198f9e0c00986f2",
+    "url": "/WhenDoesItRelease/static/js/2.1f9af4b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WhenDoesItRelease/static/js/2.1f9af4b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82986302af2e91dc2483",
+    "url": "/WhenDoesItRelease/static/js/main.f760eacf.chunk.js"
+  },
+  {
+    "revision": "d4ddbec43b44a6e35d67",
+    "url": "/WhenDoesItRelease/static/js/runtime-main.df1cf092.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WhenDoesItRelease/static/media/logo.5d5d9eef.svg"
+  }
+]);
